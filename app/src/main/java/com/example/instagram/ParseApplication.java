39054f89eb921b8a9507e0d58251754d3deb2c25
Nayla1212 <nayla.boorady@gmail.com>
2021-07-02 19:37:00 -0400

    initial commit

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    //Initializes Parse SKD as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Register your parse models
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("yqkNPbWO3kCP6M6APNLcMZEwwFJryFqMM5yxCEXm")
+                .clientKey("c9TlOdnjwi8cifIYtCxliH23hDcSqJhWYRr17PH2")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
